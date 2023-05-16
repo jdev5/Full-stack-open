0a348ee447b4da107433a79e57e90b989f8ae8f1
Jonathan Fernandez <jafmjah@gmail.com>
2023-05-16 11:35:32 -0300

    firts part

---
+++ part3/backend-phonebook/index.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+
+   let persons = [
+      { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
+      },
+      { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+      },
+      { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
+      }
+    ]
+
+    
+    app.get('/info',(request, response) =>{
+    const currentTime = new Date().toISOString();
+    response.send(`Phonebook has info for ${persons.length} people.\n Request received at ${currentTime}.`)
+  })
+
+    app.get('/api/persons',(request, response) =>{
+        response.json(persons)
+    })
+    
+    app.get('/api/persons/:id', (request, response) => {
+        const id = Number(request.params.id)
+        const person = persons.find(person => person.id === id)
+        
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end()
+        }
+      })
+
+      app.delete('/api/persons/:id', (request, response) => {
+        const id = Number(request.params.id)
+        persons = persons.filter(person => person.id !== id)
+      
+        response.status(204).end()
+      })
+      
+      const generateId = () => {
+        const maxId = persons.length > 0
+          ? Math.max(...persons.map(n => n.id))
+          : 0
+        return maxId + 1
+      }
+   
+      const nameExists = (name) => {
+        return persons.some(person => person.name.toLowerCase() === name.toLowerCase())
+      }
+
+      app.post('/api/persons', (request, response) => {
+       const body = request.body
+       
+       if (!body.name) {
+            return response.status(400).json({ 
+              error: 'has not added the name' 
+            })
+        }
+       if (!body.number) {
+            return response.status(400).json({ 
+              error: 'has not added phone number' 
+            })
+        }
+
+        const name = body.name.trim()
+
+        if (nameExists(name)) {
+          return response.status(400).json({ 
+            error: 'Name already exists in the phonebook' 
+          })
+        }
+        
+        const newPerson = {
+            name: body.name,
+            number: body.number,
+            id: generateId(),
+        }
+        
+        person = persons.concat(newPerson)
+
+        response.json(newPerson)
+    }) 
+
+    const PORT = 3001
+    app.listen(PORT, () =>{
+        console.log(`Server running on port ${PORT}`)
+    })
+  
